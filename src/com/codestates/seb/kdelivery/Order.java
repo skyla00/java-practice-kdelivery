c7d7b75e3cf950f0972018c6367965ec3e8fbff3
Jungmin Lee <75019459+Lucky-kor@users.noreply.github.com>
2024-05-20 18:01:26 +0900

    Add files via upload

---
+++ src/com/codestates/seb/kdelivery/Order.java
@@ -0,0 +1,12 @@
+package com.codestates.seb.kdelivery;
+
+public class Order {
+  private String customerName;
+  private String shopName;
+  private String foodName;
+
+  /**
+  *@Order():주문 정보를 저장합니다.
+  **/
+
+}
